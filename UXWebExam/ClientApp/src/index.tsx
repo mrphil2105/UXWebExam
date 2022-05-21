9296bb96efde9942cd43c5b7dc59ff59ce38f9ea
Philip Mørch <mrphil2105@gmail.com>
2022-05-21 14:05:39 +0200

    Create index.html and index.tsx

---
+++ UXWebExam/ClientApp/src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App"
+
+const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href") as string;
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+    <BrowserRouter basename={baseUrl}>
+        <App />
+    </BrowserRouter>,
+    rootElement
+);
