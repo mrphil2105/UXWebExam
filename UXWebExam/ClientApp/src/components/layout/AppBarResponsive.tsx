6155bb0d23710340eea61af622d7145eef51cbbe
alberted <alberted115@gmail.com>
2022-05-30 15:46:44 +0200

    fixed name

--- UXWebExam/ClientApp/src/components/layout/AppBarResponsive.tsx
+++ UXWebExam/ClientApp/src/components/layout/AppBarResponsive.tsx
@@ -22,7 +22,7 @@ const pages = [
     { name: "Register", link: "/register" },
     { name: "Login", link: "/login" },
     { name: "Overview", link: "/overview" },
-    { name: "MyReservations", link: "/MyReservations" },
+    { name: "My Reservations", link: "/MyReservations" },
 ];
 const settings = ["Profile", "Account", "Logout"];
 
