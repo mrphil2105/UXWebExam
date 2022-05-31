3f64e3a6164da8d34e62dcd1a038792508104fee
Philip Mørch <mrphil2105@gmail.com>
2022-05-31 17:24:20 +0200

    Install Roboto typeface

--- UXWebExam/ClientApp/src/index.tsx
+++ UXWebExam/ClientApp/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App"
+import "typeface-roboto";
 
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href") as string;
 const container = document.getElementById("app");
