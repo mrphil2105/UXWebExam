023b37f9ba961ff586573a7f05f6d3b81110bf12
Philip Mørch <mrphil2105@gmail.com>
2022-05-31 17:03:50 +0200

    Create CarModel types

---
+++ UXWebExam/Models/CarModel.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UXWebExam.Models;
+
+public class CarModel
+{
+    public int Id { get; init; }
+
+    [Required]
+    public string? Name { get; init; }
+
+    [Required]
+    public string? Type { get; init; }
+
+    [Required]
+    public decimal Price { get; init; }
+
+    public string? ImageUrl { get; init; }
+
+    public string? Street { get; init; }
+
+    public string? HouseNumber { get; init; }
+
+    public int PostalCode { get; init; }
+
+    public string? City { get; init; }
+
+    [Required]
+    public double Longitude { get; init; }
+
+    [Required]
+    public double Latitude { get; init; }
+}
