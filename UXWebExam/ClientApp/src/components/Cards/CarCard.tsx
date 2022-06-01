706641a3ace06a53cd29511a7a7b87cfe5a3342d
alberted <alberted115@gmail.com>
2022-06-01 09:15:07 +0200

    added newer carCard version

--- UXWebExam/ClientApp/src/components/Cards/CarCard.tsx
+++ UXWebExam/ClientApp/src/components/Cards/CarCard.tsx
@@ -4,30 +4,36 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import { Car } from '../../classes/Car';
+import CarModel from "../../models/CarModel";
 
+interface Props {
+    car: CarModel;
+}
 
-export default function CarCard({car}:any) {
-  return (
-    <Card sx={{}}>
-      <CardActionArea>
-      <Typography gutterBottom variant="h4" component="div" padding={1}>
-            {car.name}
-      </Typography>
-        <CardMedia
-          component="img"
-          height="300"
-          image={car.imageURL}
-          alt={car.imageURL}
-        />
-        <CardContent>
-          <Typography variant="body1" color="text.secondary">
-            {car.type}
-            <br/>
-            {car.price} kr./dagen
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  );
+export default function CarCard(props: Props) {
+    return (
+        <Card>
+            <CardActionArea>
+                <Typography gutterBottom variant="h4" component="div" padding={1}>
+                    {props.car.name}
+                </Typography>
+                <CardMedia
+                    component="img"
+                    height="300"
+                    image={props.car.imageUrl}
+                    alt={props.car.imageUrl}
+                />
+                <CardContent>
+                    <Typography variant="body1" color="text.secondary">
+                        <br/>
+                        {props.car.street} {props.car.houseNumber}, {props.car.postalCode} {props.car.city}
+                        <br/>
+                        {props.car.type}
+                        <br />
+                        {props.car.price} kr./dagen
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
 }
