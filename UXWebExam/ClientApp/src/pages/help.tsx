8575e80eeda33d0201656946be93c6271a3f1a6a
alberted <alberted115@gmail.com>
2022-06-02 23:16:32 +0200

    updated help text

--- UXWebExam/ClientApp/src/pages/help.tsx
+++ UXWebExam/ClientApp/src/pages/help.tsx
@@ -9,22 +9,21 @@ export default function Help() {
                 <Typography variant="h4" sx={{color:'primary.dark'}}>How to use website</Typography>
                 <Box component="span" sx={{ p: 2, border: 2,  borderColor:'primary.main',borderRadius: 1 }}>
                     <Typography variant="h5">1. Pick a car</Typography>
-                    <Typography>Pick a car near you, that you want to drive. You can see cars on a map, or in a list of cars.</Typography>
+                    <Typography>Pick a car near you, that you want to drive. You can see cars on the map, or on the search page.</Typography>
                 </Box>
                 <Box component="span" sx={{ p: 2, border: 2,  borderColor:'primary.main',borderRadius: 1 }}>
                     <Typography variant="h5">2. Book</Typography>
-                    <Typography>Once you have picked a car, you can book it between a given start and end date and time.</Typography>
+                    <Typography>Once you have picked a car, you can book it between a given start and end date.</Typography>
                 </Box>
                 <Box component="span" sx={{ p: 2, border: 2,  borderColor:'primary.main',borderRadius: 1 }}>
                     <Typography variant="h5">3. Drive</Typography>
                     <Typography>
                         When your booking starts, you can find the car at the given address.
                         You must deliver the car back to the same address, before your booking ends.
-                        If you don't do this you will be charged extra. Once you've the delivered the car, find the
-                        booking on the website and press "Deliver".
+                        If you don't do this you will be charged extra. 
                     </Typography>
                     </Box>
-                <Link to="/">
+                <Link to="/" style={{ textDecoration: 'none' }}>
                     <Button variant="contained">Understood</Button>
                 </Link>
             </Stack>
