1104658d09672031065126f4013349715ae50794
Philip Mørch <mrphil2105@gmail.com>
2022-06-02 20:10:22 +0200

    Remove profile icon from menu

--- UXWebExam/ClientApp/src/components/layout/AppBarResponsive.tsx
+++ UXWebExam/ClientApp/src/components/layout/AppBarResponsive.tsx
@@ -203,46 +203,6 @@ const ResponsiveAppBar = () => {
                             ))}
                             <LoginMenu isMobile={false} />
                         </Box>
-
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton
-                                    onClick={handleOpenUserMenu}
-                                    sx={{ p: 0 }}
-                                >
-                                    <Avatar sx={{bgcolor:theme.palette.primary.main}}>
-                                        <AccountCircleIcon sx={{ color:theme.palette.secondary.main , width:"100%", height:"100%"}} />
-                                    </Avatar>
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: "45px" }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: "top",
-                                    horizontal: "right",
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: "top",
-                                    horizontal: "right",
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting) => (
-                                    <MenuItem
-                                        key={setting}
-                                        onClick={handleCloseUserMenu}
-                                    >
-                                        <Typography textAlign="center">
-                                            {setting}
-                                        </Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
                     </Toolbar>
                 </Container>
             </AppBar>
