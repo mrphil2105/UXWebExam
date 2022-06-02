1224975ceb19b3de00818646db5b448d9b4389e4
Philip Mørch <mrphil2105@gmail.com>
2022-06-02 21:06:30 +0200

    Create BookCard component

---
+++ UXWebExam/ClientApp/src/components/cards/BookCard.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CarModel from "../../models/CarModel";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LocalGasStationIcon from "@mui/icons-material/LocalGasStation";
+import { CardHeader } from "@mui/material";
+
+interface Props {
+    car: CarModel;
+}
+
+export default function BookCard(props: Props) {
+    return (
+        <Card>
+            <CardHeader title={props.car.name} sx={{ backgroundColor: "primary.main", color: "white" }} />
+            <CardMedia component="img" image={props.car.imageUrl} alt={props.car.name} />
+            <CardContent>
+                <Typography>{props.car.description}</Typography>
+                <Typography color="text.secondary">
+                    <LocationOnIcon />{props.car.street} {props.car.houseNumber}, {props.car.postalCode} {props.car.city}
+                </Typography>
+                <Typography color="text.secondary">
+                    <LocalGasStationIcon />{props.car.type}
+                </Typography>
+                <Typography color="text.secondary">
+                    <AttachMoneyIcon />{props.car.price} kr./dagen
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+}
